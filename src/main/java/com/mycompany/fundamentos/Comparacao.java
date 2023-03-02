d92d491177b752a595704d0893975437d67b2bd6
XandyGomes <x3.computadores@hotmail.com>
2023-03-02 17:09:59 -0300

    Aula.28.02.2023

---
+++ src/main/java/com/mycompany/fundamentos/Comparacao.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.fundamentos;
+import java.util.Scanner;
+/**
+ *
+ * @author Alexandre
+ */
+public class Comparacao {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int number1, number2;
+        
+        System.out.print("Digite o primeiro numero: ");
+        number1 = input.nextInt();
+        
+        
+        
+    }
+}

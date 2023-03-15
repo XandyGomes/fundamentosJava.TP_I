d1d26ec8ab03fb03b6f555753e5ddea96569c3cc
XandyGomes <x3.computadores@hotmail.com>
2023-03-14 22:24:54 -0300

    Aula.14.03.2023

--- fundamentos/src/main/java/com/mycompany/fundamentos/Temperatura.java
+++ fundamentos/src/main/java/com/mycompany/fundamentos/Temperatura.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.fundamentos;
+import java.util.Locale;
 import java.util.Scanner;
+
 
 /**
  *
@@ -11,6 +13,7 @@ import java.util.Scanner;
  */
 public class Temperatura {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
         
         //(ºF - 32) x 5/9 = ºC
@@ -18,9 +21,8 @@ public class Temperatura {
         final double FATOR = 5.0 / 9.0;
         
         System.out.print("Digite o valor em fahrenheit ºF : ");
-        double fahrenheit = Double.parseDouble(input.nextLine());
-        
-        double celsius = (fahrenheit - AJUSTE) * FATOR;
+        double fahrenheit = input.nextDouble();
+          double celsius = (fahrenheit - AJUSTE) * FATOR;
         
         System.out.println("O resultado é " + celsius + "ºC.");
         
